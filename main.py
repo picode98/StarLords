4a2c20b538237652605da37704a84efd8ccd91dc
picode98 <saaman1377@gmail.com>
2024-08-11 20:36:14 -0400

    Fixes to collision detection; add missing components to main.py
    -Bounces which involve multiple bricks at once are still not handled consistently.

--- main.py
+++ main.py
@@ -5,18 +5,34 @@ import starlords
 
 sleep = time.sleep_us if hasattr(time, 'sleep_us') else lambda us: time.sleep(us / 1e6)
 
-import machine
-# game_disp = display.PrintDisplay(20, 20)
-game_disp = display.NeopixelDisplay(machine.Pin(16), 16, 16)
-game = starlords.StarlordsGame(game_disp)
-game._state.ball_velocity = (1.01, 1.0)
+DISPLAY_SIZE = (16, 16)
 
-target_ticks = 100000000
-ticks = time.time_ns()
+if display.NeopixelDisplay is None:
+    game_disp = display.PrintDisplay(DISPLAY_SIZE[0], DISPLAY_SIZE[1])
+else:
+    import machine
+    game_disp = display.NeopixelDisplay(machine.Pin(16), DISPLAY_SIZE[0], DISPLAY_SIZE[1])
+
+game = starlords.StarlordsGame(game_disp)
+game._state.ball_velocity = (5.01, 1.0)
+
+target_ticks = 1000000000 // 10
 frame = 0
+ticks = time.time_ns()
 while True:
     frame_time = target_ticks / 1.0e9
     while frame_time > 0.0:
-        frame_time -= game.update(0.1)
+        frame_time -= game.update(frame_time)
+
+    # print(f'Frame {frame}:')
+    game.render()
+    # print()
+
+    frame_ticks = time.time_ns() - ticks
+    if frame_ticks > target_ticks:
+        print(f'WARNING: Below target frame rate (frame took {frame_ticks} ns).')
+    else:
+        sleep((target_ticks - frame_ticks) // 1000)
 
     frame += 1
+    ticks = time.time_ns()
--- starlords.py
+++ starlords.py
@@ -1,13 +1,14 @@
+import math
+
 from display import Display
 
-from math import ceil
 
 class GameState:
     """ information about which bricks are broken, projectile position, etc. """
     def __init__(self, field_size):
         w, h = field_size
         self.field_size = field_size
-        self.ball_position = (0.0, 0.0)
+        self.ball_position = (w / 2.0, h / 2.0)
         self.ball_velocity = (0.0, 0.0)
         self.castle_bricks = [[(3.0, 0.0), (3.0, 1.0), (3.0, 2.0), (3.0, 3.0), (2.0, 3.0), (1.0, 3.0), (0.0, 3.0)],
                               [(w - 4.0, 0.0), (w - 4.0, 1.0), (w - 4.0, 2.0), (w - 4.0, 3.0), (w - 3.0, 3.0), (w - 2.0, 3.0), (w - 1.0, 3.0)],
@@ -25,7 +26,8 @@ class BallColliderType:
 class StarlordsGame:
     BALL_COLOR = (50, 50, 50)
     BRICK_COLOR = (0, 0, 50)
-    BALL_SIZE = (1.0, 1.0)
+    BRICK_SIZE = (1.0, 1.0)
+    BALL_SIZE = 1.0
     MAX_DELTA = 0.3
 
     def __init__(self, display: Display):
@@ -34,17 +36,58 @@ class StarlordsGame:
 
     def _get_ball_collisions(self):
         colliders = []
-        if (self._state.ball_position[0] + self.BALL_SIZE[0] / 2.0) >= self._state.field_size[0]:
-            colliders.append((BallColliderType.WALL, (-1.0, 0.0)))
+        if (self._state.ball_position[0] + self.BALL_SIZE / 2.0) >= self._state.field_size[0]:
+            colliders.append((BallColliderType.WALL, (-1.0, 0.0), None, None))
 
-        if (self._state.ball_position[0] - self.BALL_SIZE[0] / 2.0) <= 0.0:
-            colliders.append((BallColliderType.WALL, (1.0, 0.0)))
+        if (self._state.ball_position[0] - self.BALL_SIZE / 2.0) <= 0.0:
+            colliders.append((BallColliderType.WALL, (1.0, 0.0), None, None))
 
-        if (self._state.ball_position[1] + self.BALL_SIZE[1] / 2.0) >= self._state.field_size[1]:
-            colliders.append((BallColliderType.WALL, (0.0, -1.0)))
+        if (self._state.ball_position[1] + self.BALL_SIZE / 2.0) >= self._state.field_size[1]:
+            colliders.append((BallColliderType.WALL, (0.0, -1.0), None, None))
 
-        if (self._state.ball_position[1] - self.BALL_SIZE[1] / 2.0) <= 0.0:
-            colliders.append((BallColliderType.WALL, (0.0, 1.0)))
+        if (self._state.ball_position[1] - self.BALL_SIZE / 2.0) <= 0.0:
+            colliders.append((BallColliderType.WALL, (0.0, 1.0), None, None))
+
+        for player_index, brick_list in enumerate(self._state.castle_bricks):
+            for brick_index, (brick_x, brick_y) in enumerate(brick_list):
+                corners = [(brick_x, brick_y), (brick_x + StarlordsGame.BRICK_SIZE[0], brick_y),
+                           (brick_x + StarlordsGame.BRICK_SIZE[0], brick_y + StarlordsGame.BRICK_SIZE[1]),
+                           (brick_x, brick_y + StarlordsGame.BRICK_SIZE[1])]
+                closest_corner, dist = min(((corner, (corner[0] - self._state.ball_position[0]) ** 2 + (corner[1] - self._state.ball_position[1]) ** 2)
+                    for corner in corners), key=lambda x: x[1])
+                dist = math.sqrt(dist)
+
+                if dist <= self.BALL_SIZE / 2.0:
+                    abs_intersect_x = math.sqrt((StarlordsGame.BALL_SIZE / 2.0) ** 2 - (closest_corner[1] - self._state.ball_position[1]) ** 2)
+                    abs_intersect_y = math.sqrt((StarlordsGame.BALL_SIZE / 2.0) ** 2 - (closest_corner[0] - self._state.ball_position[0]) ** 2)
+                    ix_point = (self._state.ball_position[0] + (-abs_intersect_x if closest_corner[0] < self._state.ball_position[0] else abs_intersect_x), closest_corner[1])
+                    iy_point = (closest_corner[0], self._state.ball_position[1] + (-abs_intersect_y if closest_corner[1] < self._state.ball_position[1] else abs_intersect_y))
+
+                    normal_vector = (self._state.ball_position[0] - (ix_point[0] + iy_point[0]) / 2.0,
+                                     self._state.ball_position[1] - (ix_point[1] + iy_point[1]) / 2.0)
+                    # normal_vector = (self._state.ball_position[0] - closest_corner[0],
+                    #                  self._state.ball_position[1] - closest_corner[1])
+                    length = math.sqrt(normal_vector[0] ** 2 + normal_vector[1] ** 2)
+                    unit_normal_vector = (normal_vector[0] / length, normal_vector[1] / length)
+                    colliders.append((BallColliderType.CASTLE_BRICK, unit_normal_vector, player_index, brick_index))
+                elif (self._state.ball_position[0] + StarlordsGame.BALL_SIZE / 2.0 >= brick_x and
+                         self._state.ball_position[0] - StarlordsGame.BALL_SIZE / 2.0 <= brick_x + StarlordsGame.BRICK_SIZE[0] and
+                         self._state.ball_position[1] >= brick_y and
+                         self._state.ball_position[1] <= brick_y + StarlordsGame.BRICK_SIZE[1]
+                    ) or (
+                        self._state.ball_position[0] >= brick_x and
+                        self._state.ball_position[0] <= brick_x + StarlordsGame.BRICK_SIZE[0] and
+                        self._state.ball_position[1] + StarlordsGame.BALL_SIZE / 2.0 >= brick_y and
+                        self._state.ball_position[1] - StarlordsGame.BALL_SIZE / 2.0 <= brick_y + StarlordsGame.BRICK_SIZE[1]
+                    ):
+                    delta = (self._state.ball_position[0] - (brick_x + StarlordsGame.BRICK_SIZE[0] / 2.0),
+                             self._state.ball_position[1] - (brick_y + StarlordsGame.BRICK_SIZE[1] / 2.0))
+                    if abs(delta[0]) >= abs(delta[1]):
+                        normal_vector = (-1.0 if delta[0] < 0.0 else 1.0, 0.0)
+                    else:
+                        normal_vector = (0.0, -1.0 if delta[1] < 0.0 else 1.0)
+
+                    colliders.append((BallColliderType.CASTLE_BRICK, normal_vector, player_index, brick_index))
 
         return colliders
 
@@ -54,10 +97,19 @@ class StarlordsGame:
         if mag_delta_sqr >= StarlordsGame.MAX_DELTA ** 2:
             time_delta /= (mag_delta_sqr / (StarlordsGame.MAX_DELTA ** 2))
 
-        for coll_type, normal_vector in self._get_ball_collisions():
-            dot_product = self._state.ball_velocity[0] * normal_vector[0] + self._state.ball_velocity[1] * normal_vector[1]
-            proj_velocity = (dot_product * normal_vector[0], dot_product * normal_vector[1])
-            reflected_proj_velocity = (abs(dot_product) * normal_vector[0], abs(dot_product) * normal_vector[1])
+        collisions = self._get_ball_collisions()
+        avg_normal_vector = (0.0, 0.0)
+        for _, normal_vector, _, _ in collisions:
+            avg_normal_vector = (avg_normal_vector[0] + normal_vector[0], avg_normal_vector[1] + normal_vector[1])
+
+        if len(collisions) > 0:
+            len_avg_normal_vector = math.sqrt(avg_normal_vector[0] ** 2 + avg_normal_vector[1] ** 2)
+            avg_normal_vector = (avg_normal_vector[0] / len_avg_normal_vector, avg_normal_vector[1] / len_avg_normal_vector)
+
+            # for coll_type, normal_vector, player_index, brick_index in self._get_ball_collisions():
+            dot_product = self._state.ball_velocity[0] * avg_normal_vector[0] + self._state.ball_velocity[1] * avg_normal_vector[1]
+            proj_velocity = (dot_product * avg_normal_vector[0], dot_product * avg_normal_vector[1])
+            reflected_proj_velocity = (abs(dot_product) * avg_normal_vector[0], abs(dot_product) * avg_normal_vector[1])
             self._state.ball_velocity = (self._state.ball_velocity[0] - proj_velocity[0] + reflected_proj_velocity[0],
                                          self._state.ball_velocity[1] - proj_velocity[1] + reflected_proj_velocity[1])
 
@@ -77,5 +129,5 @@ class StarlordsGame:
                 self._display[round(x), round(y)] = StarlordsGame.BRICK_COLOR
 
         # print(f'Position: {self._state.ball_position} Velocity: {self._state.ball_velocity}')
-        self._display[round(self._state.ball_position[0] - StarlordsGame.BALL_SIZE[0] / 2.0), round(self._state.ball_position[1] - StarlordsGame.BALL_SIZE[0] / 2.0)] = StarlordsGame.BALL_COLOR
+        self._display[round(self._state.ball_position[0] - StarlordsGame.BALL_SIZE / 2.0), round(self._state.ball_position[1] - StarlordsGame.BALL_SIZE / 2.0)] = StarlordsGame.BALL_COLOR
         self._display.write()
