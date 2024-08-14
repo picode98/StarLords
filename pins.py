7fe1e1b5e23b154e60a7427e916957a24361626d
DrFunn1 <ed@drfunn.com>
2024-08-13 23:13:38 -0400

    AE+ pinouts

---
+++ pins.py
@@ -0,0 +1,43 @@
+from micropython import const
+
+""" Dig-Quad AE+ pinouts """
+
+LED_BIGPIXEL_PIN = const(15)
+LED_C1_PIN = const(16)
+LED_C2_PIN = const(3)
+LED_C3_PIN = const(1)
+LED_C4_PIN = const(4)
+
+I2S_BCLK_PIN = const(21)
+I2S_WSEL_PIN = const(17)
+I2S_DIN_PIN = const(22)
+
+I2C_SDA_PIN = const(15)
+I2C_SCL_PIN = const(12)
+
+BUTTON_DEBOUNCE_MS = const(5)
+
+""" Actual pinouts below to be determined """
+BUTTON_C1_PIN = const(33)
+BUTTON_C2_PIN = const(33)
+BUTTON_C3_PIN = const(33)
+BUTTON_C4_PIN = const(33)
+BUTTON_C5_PIN = const(33)
+
+CLICK_C1_PIN = const(33)
+CLICK_C2_PIN = const(33)
+CLICK_C3_PIN = const(33)
+CLICK_C4_PIN = const(33)
+CLICK_C5_PIN = const(33)
+
+DIR_C1_PIN = const(33)
+DIR_C2_PIN = const(33)
+DIR_C3_PIN = const(33)
+DIR_C4_PIN = const(33)
+DIR_C5_PIN = const(33)
+
+PRESENCE_C1_PIN = const(33)
+PRESENCE_C2_PIN = const(33)
+PRESENCE_C3_PIN = const(33)
+PRESENCE_C4_PIN = const(33)
+
