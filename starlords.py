a5fbe93235c5f5c64b21be755ac569e0154dda2c
picode98 <saaman1377@gmail.com>
2024-08-22 21:23:12 -0400

    Add initial player shields (not yet functional) and destruction of castle bricks

--- starlords.py
+++ starlords.py
@@ -72,6 +72,8 @@ class GameState:
                               [v2(w - 4.0, h - 1.0), v2(w - 4.0, h - 2.0), v2(w - 4.0, h - 3.0), v2(w - 4.0, h - 4.0), v2(w - 3.0, h - 4.0), v2(w - 2.0, h - 4.0), v2(w - 1.0, h - 4.0)],
                               [v2(3.0, h - 1.0), v2(3.0, h - 2.0), v2(3.0, h - 3.0), v2(3.0, h - 4.0), v2(2.0, h - 4.0), v2(1.0, h - 4.0), v2(0.0, h - 4.0)]
         ]
+        self.shield_positions = [v2(5.0, 5.0), v2(w - 6.0, 5.0), v2(w - 6.0, h - 6.0), v2(5.0, h - 6.0)]
+        self.explosions = []
 
 
 class BallColliderType:
@@ -83,9 +85,13 @@ class BallColliderType:
 class StarlordsGame:
     BALL_COLOR = (50, 50, 50)
     BRICK_COLOR = (0, 0, 50)
+    SHIELD_COLOR = (50, 0, 0)
+    EXPLOSION_COLOR = (50, 50, 50)
     BRICK_SIZE = v2(1.0, 1.0)
     BALL_SIZE = 1.0
     MAX_DELTA = 0.3
+    MINIMUM_EXPLOSION_BRIGHTNESS = 0.05
+    EXPLOSION_SHOCKWAVE_VELOCITY = 8.0
 
     def __init__(self, display: Display):
         self._state = GameState(v2(display.width, display.height))
@@ -152,12 +158,31 @@ class StarlordsGame:
         if mag_delta_sqr >= StarlordsGame.MAX_DELTA ** 2:
             time_delta /= (mag_delta_sqr / (StarlordsGame.MAX_DELTA ** 2))
 
+        radius_delta = StarlordsGame.EXPLOSION_SHOCKWAVE_VELOCITY * time_delta
+        new_explosions = []
+        for position, radius, brightness in self._state.explosions:
+            new_explosion = (position, radius + radius_delta, brightness * ((radius / (radius + radius_delta)) ** 2))
+            if new_explosion[2] >= StarlordsGame.MINIMUM_EXPLOSION_BRIGHTNESS:
+                new_explosions.append(new_explosion)
+
+        self._state.explosions = new_explosions
+
         collisions = self._get_ball_collisions()
-        avg_normal_vector = sum((normal_vector for _, normal_vector, _, _ in collisions), start=v2(0.0, 0.0))
+        avg_normal_vector = sum((normal_vector for _, normal_vector, _, _ in collisions), v2(0.0, 0.0))
         # for _, normal_vector, _, _ in collisions:
         #     avg_normal_vector = (avg_normal_vector[0] + normal_vector[0], avg_normal_vector[1] + normal_vector[1])
 
         if len(collisions) > 0:
+            brick_removals = set()
+            for coll_type, normal_vector, player_index, brick_index in collisions:
+                if coll_type == BallColliderType.CASTLE_BRICK:
+                    brick_pos = self._state.castle_bricks[player_index][brick_index]
+                    brick_removals.add((player_index, brick_index))
+                    self._state.explosions.append((brick_pos + StarlordsGame.BRICK_SIZE / 2, 1.0, 5.0))
+
+            self._state.castle_bricks = [[brick_pos for brick_index, brick_pos in enumerate(brick_list) if not (player_index, brick_index) in brick_removals]
+                                         for player_index, brick_list in enumerate(self._state.castle_bricks)]
+
             avg_normal_vector = avg_normal_vector / avg_normal_vector.length()
 
             # for coll_type, normal_vector, player_index, brick_index in self._get_ball_collisions():
@@ -179,6 +204,17 @@ class StarlordsGame:
         for brick_list in self._state.castle_bricks:
             for brick_pos in brick_list:
                 self._display[round(brick_pos.x), round(brick_pos.y)] = StarlordsGame.BRICK_COLOR
+
+        for position, radius, brightness in self._state.explosions:
+            for block_x in range(max(int(position.x - radius), 0), min(math.ceil(position.x + radius) + 1, self._display.width - 1)):
+                for block_y in range(max(int(position.y - radius), 0), min(math.ceil(position.y + radius) + 1, self._display.height - 1)):
+                    if (position.x - (block_x + 0.5)) ** 2 + (position.y - (block_y + 0.5)) ** 2 <= radius:
+                        self._display[block_x, block_y] = (round(StarlordsGame.EXPLOSION_COLOR[0] * brightness),
+                                                           round(StarlordsGame.EXPLOSION_COLOR[1] * brightness),
+                                                           round(StarlordsGame.EXPLOSION_COLOR[2] * brightness))
+
+        for shield_pos in self._state.shield_positions:
+            self._display[round(shield_pos.x), round(shield_pos.y)] = StarlordsGame.SHIELD_COLOR
 
         # print(f'Position: {self._state.ball_position} Velocity: {self._state.ball_velocity}')
         self._display[round(self._state.ball_position.x - StarlordsGame.BALL_SIZE / 2.0), round(self._state.ball_position.y - StarlordsGame.BALL_SIZE / 2.0)] = StarlordsGame.BALL_COLOR
