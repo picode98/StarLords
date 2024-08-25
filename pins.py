cce7c667dfcc552a7d7ab1f18f64b4e5e44c1504
DrFunn1 <43909615+DrFunn1@users.noreply.github.com>
2024-08-24 23:55:20 -0400

    Update pins.py for raspberry pi breakout board

--- pins.py
+++ pins.py
@@ -1,43 +1,50 @@
-from micropython import const
+""" 
+pins.py
+Raspberry Pi 3B+ pinouts 
+GPIO.setmode(GPIO.BCM) broadcom chip pin numbers
+as labeled on xikentech breakout board with screw terminals
+note idsc is gpio 0 and idsd is gpio 1
+note that pin logic levels are at 3.3 volts not 5 volts
+that should be okay for all inputs; rotary encoder, PIR and cap touch sensors
+however the five addressable LED data lines need level shifted to 5 volts, 
+possibly with a 33 ohm resistor in series to compensate for the two meter transmision line
+pin numbering scheme is to make wiring on the breakout board consistent as possible
+Five rotary encoders with pushbutton; four control towers and a fifth one for 
+control box (audio volume control towers, audio volume sub woofers, bigpixel display brightness and game speed)
+"""
 
-""" Dig-Quad AE+ pinouts """
+"""
+5 volt level shifted outputs
+"""
+LED_BIGPIXEL_PIN = const(17)
+LED_C1_PIN = const(7)
+LED_C2_PIN = const(16)
+LED_C3_PIN = const(19)
+LED_C4_PIN = const(26)
 
-LED_BIGPIXEL_PIN = const(15)
-LED_C1_PIN = const(16)
-LED_C2_PIN = const(3)
-LED_C3_PIN = const(1)
-LED_C4_PIN = const(4)
+"""
+3.3 volt sensor inputs
+"""
+BUTTON_C1_PIN = const(8)
+BUTTON_C2_PIN = const(6)
+BUTTON_C3_PIN = const(20)
+BUTTON_C4_PIN = const(13)
+BUTTON_C5_PIN = const(2)
 
-I2S_BCLK_PIN = const(21)
-I2S_WSEL_PIN = const(17)
-I2S_DIN_PIN = const(22)
+CLICK_C1_PIN = const(11)
+CLICK_C2_PIN = const(5)
+CLICK_C3_PIN = const(12)
+CLICK_C4_PIN = const(21)
+CLICK_C5_PIN = const(3)
 
-I2C_SDA_PIN = const(15)
-I2C_SCL_PIN = const(12)
+DIR_C1_PIN = const(9)
+DIR_C2_PIN = const(25)
+DIR_C3_PIN = const(0)
+DIR_C4_PIN = const(27)
+DIR_C5_PIN = const(23)
 
-BUTTON_DEBOUNCE_MS = const(5)
-
-""" Actual pinouts below to be determined """
-BUTTON_C1_PIN = const(33)
-BUTTON_C2_PIN = const(33)
-BUTTON_C3_PIN = const(33)
-BUTTON_C4_PIN = const(33)
-BUTTON_C5_PIN = const(33)
-
-CLICK_C1_PIN = const(33)
-CLICK_C2_PIN = const(33)
-CLICK_C3_PIN = const(33)
-CLICK_C4_PIN = const(33)
-CLICK_C5_PIN = const(33)
-
-DIR_C1_PIN = const(33)
-DIR_C2_PIN = const(33)
-DIR_C3_PIN = const(33)
-DIR_C4_PIN = const(33)
-DIR_C5_PIN = const(33)
-
-PRESENCE_C1_PIN = const(33)
-PRESENCE_C2_PIN = const(33)
-PRESENCE_C3_PIN = const(33)
-PRESENCE_C4_PIN = const(33)
+PRESENCE_C1_PIN = const(10)
+PRESENCE_C2_PIN = const(24)
+PRESENCE_C3_PIN = const(1)
+PRESENCE_C4_PIN = const(4)
 
