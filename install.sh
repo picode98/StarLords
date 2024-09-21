6ead1f8b75708a5f4f91df524bef9c51d5326987
picode98 <saaman1377@gmail.com>
2024-09-21 18:24:52 -0400

    Fixes to install script

--- install.sh
+++ install.sh
@@ -7,16 +7,17 @@ apt-get upgrade -y
 echo "Installing StarLords dependencies..."
 apt-get install -y --upgrade python3-venv python3-setuptools
 python3 -m venv ./venv --system-site-packages
-./venv/bin/pip3 install --upgrade adafruit-python-shell
+source ./venv/bin/activate
+pip3 install --upgrade adafruit-python-shell
 
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
-./venv/bin/python3 ./raspi-blinka.py
+python3 ./raspi-blinka.py
 apt-get install -y python3-rpi-lgpio
-./venv/bin/pip3 uninstall rpi-lgpio
+pip3 uninstall rpi-lgpio
 
 echo "Configuring network interfaces..."
-nmcli con mod eth0 ipv4.method manual ipv4.addresses 10.0.0.2/24 ipv4.gateway 10.0.0.1 ipv4.dns 10.0.0.1
-nmcli con mod eth0 ipv4.route-metric 200
+nmcli con mod "Wired connection 1" ipv4.method manual ipv4.addresses 10.0.0.2/24 ipv4.gateway 10.0.0.1 ipv4.dns 10.0.0.1
+nmcli con mod "Wired connection 1" ipv4.route-metric 200
 systemctl restart NetworkManager
 rfkill block all
 
